7e3bd437307408f6ba10d85dc8ad88d92e1f1e7e
Hans Olav Hygen <hohygen@gmail.com>
2016-12-16 15:19:12 +0100

    Update GlobalStatus.R
    New version

--- GlobalStatus.R
+++ GlobalStatus.R
@@ -2,29 +2,35 @@
 #developments of global temperature describes the possible outcomes of this year. The plot is a bit strange since whats plotted in month 1 is january data, 
 #month two is average of january and february, month three january, february, and march data ending in month 12 with yearly averages.
 
-GStatus <- function(Y=2016,M=3){ #Use Y for the year of yor analysis, and M for the month of interrest
-  GData <- read.csv("http://data.giss.nasa.gov/gistemp/tabledata_v3/GLB.Ts+dSST.csv",na.strings = c("***","****"),colClasses = "numeric")
+GStatus <- function(Y=2016,M=3){
+  GData <- read.csv("http://data.giss.nasa.gov/gistemp/tabledata_v3/GLB.Ts+dSST.csv",na.strings = c("***","****"),colClasses = "numeric", skip=1)
+  print(GData)
   D2 <- GData[,1:2]
   for (n in 2:12){
-    print(n)
-    print(dim(D2))
+    #print(n)
+    #print(dim(D2))
     D2 <- cbind(D2,
                 rowMeans(GData[,2:(n+1)]))
-    print(dim(D2))
+    #print(dim(D2))
   }
-  plot(1:12,D2[D2[,1]==Y,2:13]/100,type="l",ylim=c(-1,1.5),xlab="Måned",ylab="Temperaturavvik",main="NASA - GISS")
+#  plot(1:12,D2[D2[,1]==Y,2:13]/100,type="l",ylim=c(-1,1.5),xlab="Måned",ylab="Temperaturavvik",main="NASA - GISS")
+  plot(1:12,D2[D2[,1]==Y,2:13],type="l",ylim=c(-1,1.5),xlab="Måned",ylab="Temperaturavvik",main="NASA - GISS")
   NY <- length(D2[,1])
   for (n in 1:NY){
-    lines(c(1:12),D2[n,2:13]/100,col="gray")
+#    lines(c(1:12),D2[n,2:13]/100,col="gray")
+    lines(c(1:12),D2[n,2:13],col="gray")
   }
-  
-  lines(1:12,D2[D2[,1]==(Y-1),2:13]/100,col="blue",lwd=2)
+
+#  lines(1:12,D2[D2[,1]==(Y-1),2:13]/100,col="blue",lwd=2)
+  lines(1:12,D2[D2[,1]==(Y-1),2:13],col="blue",lwd=2)
   D3 <- D2
   NY <- length(D3[,1])
   for (n in 1:NY){
     D3[n,c(M:12)+1]<-D3[n,c(M:12)+1]-(D3[n,M+1]-D3[NY,M+1])
-    lines(c(M:12),D3[n,c(M:12)+1]/100,col="orange")
+#    lines(c(M:12),D3[n,c(M:12)+1]/100,col="orange")
+    lines(c(M:12),D3[n,c(M:12)+1],col="orange")
   }
-  lines(1:12,D2[D2[,1]==Y,2:13]/100,col="red",lwd=2)
+#  lines(1:12,D2[D2[,1]==Y,2:13]/100,col="red",lwd=2)
+  lines(1:12,D2[D2[,1]==Y,2:13],col="red",lwd=2)
   D3
 }
